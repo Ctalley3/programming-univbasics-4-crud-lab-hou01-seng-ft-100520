3acfaa8d79f6bc2072414fcbdd84c111aa912e7b
Crystal T <ctalley.cat@gmail.com>
2020-09-23 02:02:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
-end
+  []
+  end
 
 def create_an_array
-  
-end
+  [earth, water, air , fire]
+  end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  << (element)
+  end
 
 def add_element_to_start_of_array(array, element)
-  
+  unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
